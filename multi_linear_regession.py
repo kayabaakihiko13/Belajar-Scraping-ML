15f71fa03bb18e32a6b8042eb159d76b32ef77bd
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-07-09 21:49:38 +0700

    okelah,wkwkw

--- multi_linear_regession.py
+++ multi_linear_regession.py
@@ -1,43 +1,61 @@
 import numpy as np
-# https://open.spotify.com/album/4koqulx259upSRxNzOx0RT
-def costFunction(x_data:np.array
-                 ,y_data,theta:np.array):
-    hypo = np.dot(x_data,theta)
-    hypo -= y_data
-    sum_error=np.sum(hypo ** 2)
-    error= sum_error/(2*x_data.shape[0])
-    return error
 
-def gradientDencest(x_data:np.array,
-                    y_data:np.array,
-                    theta:np.array,
-                    alpha:float):
-    predict = np.dot(x_data,theta)
-    predict -= y_data
-    sum_gradient = np.dot(x_data.T,predict)
-    theta = theta - (alpha/x_data.shape[0]) *sum_gradient
-    return theta
+class linear_regression:
+    def __init__(self, iterations=1000, learning_rate=0.01, fit_intercept=True):
+        self.iterations = iterations
+        self.learning_rate = learning_rate
+        self.fit_intercept = fit_intercept
+        self.theta = None
 
-def linear_regession(x_data: np.array,y_data: np.array,
-                     alpha :float=0.00001,
-                     iterable :int=1000,
-                     intrcept :bool=True):
-    X = np.array(x_data)
-    y = np.array(y_data) 
-    if intrcept:
-        X = np.c_[np.ones((X.shape[0], 1)), X]
-    _,m=X.shape
-    theta=np.zeros((m,1))
-    for i in range (iterable) :
-        theta=gradientDencest(X,y,theta,alpha)
-        error=costFunction(X,y,theta)
-        print(f'{i+1}:error{error}')
-    return theta
+    def __cost_function(self, X, y):
+        m = len(y)
+        h = np.dot(X, self.theta)
+        error = (1 / (2 * m)) * np.sum(np.square(h - y))
+        return error
+
+    def __gradient_descent(self, X, y):
+        m = len(y)
+        h = np.dot(X, self.theta)
+        gradient = (1 / m) * np.dot(X.T, (h - y))
+        self.theta = self.theta - self.learning_rate * gradient
+        return self.theta
+
+    def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        
+        if self.fit_intercept:
+            ones = np.ones((X.shape[0], 1))
+            X = np.concatenate((ones, X), axis=1)
+
+        self.theta = np.zeros(X.shape[1])
+
+        for _ in range(self.iterations):
+            self.theta=self.__gradient_descent(X, y)
+            cost = self.__cost_function(X, y)
+            print(f"Cost: {cost}")
+
+        return self
+
+    def predict(self, X):
+        X = np.array(X)
+
+        if self.fit_intercept:
+            ones = np.ones((X.shape[0], 1))
+            X = np.concatenate((ones, X), axis=1)
+
+        return np.dot(X, self.theta)
+
 
 if __name__ == "__main__":
-    x = np.array([[21, 2, 1, 2],
-                  [12, 3, 4, 2]])
-    y = np.array([[2], [2]])
-    linear_regession(x, y)
+    import matplotlib.pyplot as plt
+    x = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(x, np.array([1, 2])) + 3
 
+    model = linear_regression(iterations=1000, learning_rate=0.01)
+    model.fit(x, y)
+
+    x_new = np.array([[1, 1]])
+    y_pred = model.predict(x)
     
+    print("Predicted:", y_pred)
